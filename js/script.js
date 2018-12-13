9d6377dc55421f155e3f10b7c75bb10e678678b4
emainta <45520658+emainta@users.noreply.github.com>
2018-12-13 13:06:06 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
 
+var fs = false;
+document.getElementById('fs_btn').onclick = fullScreen;
+
+
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+  fs = !fs;
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+  fs = !fs;
+}
+
+
+function fullScreen() {
+  if (!fs) {
+     openFullscreen()
+   }
+
+  else closeFullscreen();
+}
